036a904a780d9842293cff46740aa61c1945b3e4
Tim Tran <49887921+tacascer@users.noreply.github.com>
2024-07-04 18:14:26 -0400

    Back up nix config instead of deleting it

--- setup-nix.sh
+++ setup-nix.sh
@@ -1,6 +1,3 @@
 cp /etc/nixos/hardware-configuration.nix $PWD/hardware-configuration.nix
-rm /etc/nixos/*
-ln -s $PWD/hardware-configuration.nix /etc/nixos/hardware-configuration.nix
-ln -s $PWD/configuration.nix /etc/nixos/configuration.nix
-ln -s $PWD/flake.nix /etc/nixos/flake.nix
-ln -s $PWD/flake.lock /etc/nixos/flake.lock
+sudo mv /etc/nixos /etc/nixos.bak
+sudo ln -s $PWD /etc/nixos
