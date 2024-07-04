8df67211d105ac81c99ca4c69a58d7fcd7e6ea6a
Tim Tran <49887921+tacascer@users.noreply.github.com>
2024-07-04 17:13:26 -0400

    preserve the hardware-configuration.nix

--- setup-nix.sh
+++ setup-nix.sh
@@ -1,3 +1,4 @@
+cp /etc/nixos/hardware-configuration.nix $PWD/hardware-configuration.nix
 rm /etc/nixos/*
 ln -s $PWD/hardware-configuration.nix /etc/nixos/hardware-configuration.nix
 ln -s $PWD/configuration.nix /etc/nixos/configuration.nix
