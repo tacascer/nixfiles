bb329694dcd481c81d73eae2bf5534040c29bbc5
Tim Tran <49887921+tacascer@users.noreply.github.com>
2024-07-04 16:38:01 -0400

    indentation for nvim

--- home-manager.nix
+++ home-manager.nix
@@ -1,38 +1,53 @@
 { config, pkgs, lib, ... } :
 let onePassPath = "~/.1password/agent.sock";
 in {
+	home = {
+		username = "tacascer";
+		homeDirectory = "/home/tacascer";
+	};
 	programs.ssh = {
 		enable = true;
 		extraConfig = ''
-		Host *
-		  IdentityAgent ${onePassPath}
+      Host *
+        IdentityAgent ${onePassPath}
 		'';
 		};
-	  programs.git = {
-	    enable = true;
-	    extraConfig = {
-	      init = {
-	        defaultBranch = "main";
-	      };
-	      gpg = {
-		format = "ssh";
-	      };
-	      "gpg \"ssh\"" = {
-		program = "${lib.getExe' pkgs._1password-gui "op-ssh-sign"}";
-	      };
-	      commit = {
-		gpgsign = true;
-	      };
-	      pull = {
-	        rebase = true;
-	      };
-
-	      user = {
-	        email = "49887921+tacascer@users.noreply.github.com";
-		user = "tacascer";
-		signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP4F11qhcGezqNnuicjl99tvcXdIeymu0wdPLBivoZEg";
-	      };
-	    };
-	  };
+	programs.neovim = {
+		enable = true;
+		viAlias = true;
+		vimAlias = true;
+		extraConfig = lib.strings.concatLines [
+      "set tabstop=2 shiftwidth=2"
+      "set expandtab"
+      "set autoindent"
+    ];
+	};
+  programs.git = {
+    enable = true;
+    extraConfig = {
+      init = {
+        defaultBranch = "main";
+      };
+      gpg = {
+        format = "ssh";
+      };
+      "gpg \"ssh\"" = {
+        program = "${lib.getExe' pkgs._1password-gui "op-ssh-sign"}";
+      };
+      commit = {
+        gpgsign = true;
+      };
+      pull = {
+        rebase = true;
+      };
+      user = {
+        email = "49887921+tacascer@users.noreply.github.com";
+        user = "tacascer";
+        signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP4F11qhcGezqNnuicjl99tvcXdIeymu0wdPLBivoZEg";
+      };
+    };
+  };
 	home.stateVersion = "24.05";
+	# Let home Manager install and manage itself.
+	programs.home-manager.enable = true;
 }
