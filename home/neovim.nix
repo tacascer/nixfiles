3ae8d39981349892b7c9c3f8ef9178f3f6eebf20
Tim Tran <49887921+tacascer@users.noreply.github.com>
2024-07-06 23:31:49 -0400

    switch neovim terminal to zsh

--- home/neovim.nix
+++ home/neovim.nix
@@ -7,17 +7,13 @@
     ripgrep
     fd
     luajitPackages.luarocks
+    luajit
   ];
   programs.neovim = {
     enable = true;
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
-    extraConfig = lib.strings.concatLines [
-      "set tabstop=2 shiftwidth=2"
-      "set expandtab"
-      "set autoindent"
-    ];
   };
   programs.lazygit.enable = true;
 }
