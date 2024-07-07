03bd075a1934aaeb60ffc739ea8a61148d978caa
Tim Tran <49887921+tacascer@users.noreply.github.com>
2024-07-06 21:05:30 -0400

    refactor into home/

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
         home-manager.nixosModules.home-manager {
           home-manager.useGlobalPkgs = true;	
           home-manager.useUserPackages = true;
-          home-manager.users.tacascer = import ./home.nix;
+          home-manager.users.tacascer = import ./home/default.nix;
         }
       ];
     };
