d877f0173058c7eb3eda22b3708075a1d2dfd3dd
Tim Tran <49887921+tacascer@users.noreply.github.com>
2024-07-12 12:34:31 -0400

    configure lua-language-server

--- home/neovim-config/lua/plugins/lsp_zero.lua
+++ home/neovim-config/lua/plugins/lsp_zero.lua
@@ -1,6 +1,30 @@
 return {
   { 'VonHeikemen/lsp-zero.nvim', branch = 'v3.x' },
-  { 'neovim/nvim-lspconfig' },
+  {
+    'neovim/nvim-lspconfig',
+    opts = {
+      servers = {
+        lua_ls = {
+          settings = {
+            Lua = {
+              runtime = {
+                version = 'LuaJit',
+              },
+              workspace = {
+                library = {
+                  vim.env.VIMRUNTIME,
+                  "${3rd}/luv/library"
+                },
+              },
+              hint = {
+                enable = true,
+              },
+            }
+          }
+        }
+      }
+    }
+  },
   { 'hrsh7th/cmp-nvim-lsp' },
   { 'hrsh7th/nvim-cmp' },
   { 'L3MON4D3/LuaSnip' },
