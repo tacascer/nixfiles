9317d4ab815a6f40c42f16f7b61b2c7002971449
Tim Tran <49887921+tacascer@users.noreply.github.com>
2024-07-27 11:43:26 -0400

    feat(nvim): add mini-surround

--- home/neovim-config/lua/config/lazy.lua
+++ home/neovim-config/lua/config/lazy.lua
@@ -32,6 +32,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.coding.luasnip" },
+    { import = "lazyvim.plugins.extras.coding.mini-surround" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
--- home/neovim-config/lua/plugins/lsp.lua
+++ home/neovim-config/lua/plugins/lsp.lua
@@ -25,7 +25,4 @@ return {
       },
     },
   },
-  { "hrsh7th/cmp-nvim-lsp" },
-  { "hrsh7th/nvim-cmp" },
-  { "L3MON4D3/LuaSnip" },
 }
